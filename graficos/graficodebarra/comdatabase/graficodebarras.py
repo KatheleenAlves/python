c752cedcbea195bd38df06fcc8baef94eb556446
KatheleenAlves <katheleen.alves@hotmail.com>
2024-03-10 15:11:43 -0300

    graficos

--- graficos/graficodebarra/comdatabase/graficodebarras.py
+++ graficos/graficodebarra/comdatabase/graficodebarras.py
@@ -2,7 +2,9 @@ import pandas as pd
 from IPython.display import display
 import plotly.express as px
 
-bancodedados = pd.read_excel("bd.xlsx")
+bancodedados = pd.read_excel(r"graficos\graficodebarra\comdatabase\bd.xlsx")
+
+display(bancodedados)
 
 grafico = px.bar(bancodedados, x="DESPESA", y="CUSTO", title="PONTO DE EQUILÍBRIO",  height=600, width=600)
 
